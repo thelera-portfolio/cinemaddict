2f8b21a97315ff46de2e9b7e9c46ed86956e4d6a
Lera Bilshchuk <leratheone@gmail.com>
2020-04-17 10:05:40 +0300

    Добавляет сообщение, если в системе нет фильмов

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import StatisticsComponent from "./components/statistics.js";
 import SortingComponent from "./components/sorting.js";
 import DetailsPopupComponent from "./components/details-popup.js";
 import CommentsComponent from "./components/comment.js";
+import NoFilmsComponent from "./components/no-films.js";
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
@@ -70,6 +71,50 @@ const renderCard = (filmsListContainerElement, card) => {
   cardComments.addEventListener(`click`, onCardClick);
 };
 
+const renderFilms = (filmsContainerElement, films) => {
+  const filmsListElement = new MainFilmListComponent().getElement();
+  render(siteMainElement, filmsContainerElement);
+  render(filmsContainerElement, filmsListElement);
+
+  if (FILMS_COUNT === 0) {
+    render(filmsListElement, new NoFilmsComponent().getElement());
+    return;
+  }
+
+  const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
+
+  const showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+  films.slice(0, showingFilmsCount).forEach((it) => renderCard(filmsListContainerElement, it));
+
+  const showMoreButtonComponent = new ShowMoreButtonComponent();
+  render(filmsListElement, showMoreButtonComponent.getElement());
+
+  let previousFilmsCount = showingFilmsCount;
+  showMoreButtonComponent.getElement().addEventListener(`click`, () => {
+    films.slice(previousFilmsCount, previousFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON).forEach((it) => renderCard(filmsListContainerElement, it));
+    previousFilmsCount += SHOWING_FILMS_COUNT_BY_BUTTON;
+    if (previousFilmsCount >= films.length) {
+      showMoreButtonComponent.getElement().remove();
+      showMoreButtonComponent.getElement().removeElement();
+    }
+  });
+
+  const extraFilms = ['Top rated', 'Most commented'];
+  extraFilms.forEach(title => {
+    render(filmsContainerElement, new ExtraFilmComponent(title).getElement());
+
+    const extraFilmListTitleElements = filmsContainerElement.querySelectorAll(`.films-list__title`);
+    for (const element of extraFilmListTitleElements) {
+      if (element.textContent === title) {
+        const container = element.nextElementSibling;
+        for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
+          renderCard(container, films[i]);
+        }
+      }
+    }
+  });
+};
+
 // отрисовываем звание пользователя
 render(siteHeaderElement, new RatingComponent().getElement());
 
@@ -83,45 +128,9 @@ render(navigationElement, new FilterComponent(filters).getElement(), RenderPosit
 render(siteMainElement, new SortingComponent().getElement());
 
 // отрисовываем фильмы
-const filmsContainerElement = new AllFilmsComponent().getElement();
-const allFilmsListElement = new MainFilmListComponent().getElement();
-render(siteMainElement, filmsContainerElement);
-render(filmsContainerElement, allFilmsListElement);
-
-const filmsListElement = filmsContainerElement.querySelector(`.films-list`);
-const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
-
 const films = generateCards(FILMS_COUNT);
-const showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-films.slice(0, showingFilmsCount).forEach((it) => renderCard(filmsListContainerElement, it));
-
-const showMoreButtonComponent = new ShowMoreButtonComponent();
-render(filmsListElement, showMoreButtonComponent.getElement());
-
-let previousFilmsCount = showingFilmsCount;
-showMoreButtonComponent.getElement().addEventListener(`click`, () => {
-  films.slice(previousFilmsCount, previousFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON).forEach((it) => renderCard(filmsListContainerElement, it));
-  previousFilmsCount += SHOWING_FILMS_COUNT_BY_BUTTON;
-  if (previousFilmsCount >= films.length) {
-    showMoreButtonComponent.getElement().remove();
-    showMoreButtonComponent.getElement().removeElement();
-  }
-});
-
-const extraFilms = ['Top rated', 'Most commented'];
-extraFilms.forEach(title => {
-  render(filmsContainerElement, new ExtraFilmComponent(title).getElement());
-
-  const extraFilmListTitleElements = filmsContainerElement.querySelectorAll(`.films-list__title`);
-  for (const element of extraFilmListTitleElements) {
-    if (element.textContent === title) {
-      const container = element.nextElementSibling;
-      for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
-        renderCard(container, films[i]);
-      }
-    }
-  }
-});
+const filmsContainerElement = new AllFilmsComponent().getElement();
+renderFilms(filmsContainerElement, films);
 
 //отрисовываем страницу со статистикой
 render(siteMainElement, new StatisticsComponent().getElement());
